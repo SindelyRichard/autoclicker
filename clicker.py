26b356600239dd11d318eb3ddfbefc813c4305b6
Sindely Richárd <113032480+SindelyRichard@users.noreply.github.com>
2026-03-21 00:38:40 +0100

    Add clicker.py for automated right-click actions
    This script automates right-click actions with random delays.

---
+++ clicker.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+import random
+
+print("Starting in 5 seconds...")
+time.sleep(5)
+pyautogui.click(button='right') #It casts the bait in the water
+print("Press Ctrl+c to stop the program.")
+try:
+    while True:
+        delay = random.randrange(1,6) #The delay is a random number between 1-6.
+        pyautogui.click(button='right') #It pulls it out of the water
+        pyautogui.click(button='right') #It casts it back
+        time.sleep(delay)
+except KeyboardInterrupt: #Press Ctrl+c to stop the program
+    print("Stopped.")
